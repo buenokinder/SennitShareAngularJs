39d9db1d93734ae5aa6bd52a121fbfaac34193fe
buenokinder <carlos@sennit.com.br>
2015-07-11 00:06:15 -0300

    Corrections

--- SennitShareJs.js
+++ SennitShareJs.js
@@ -1,88 +1,87 @@
-﻿'use strict';
-angular.module("sennitShareJs", []).constant('ngAuthSettings', {
-    apiServiceBaseUri: serviceBase,
-    clientId: 'ngAuthApp'
-}).factory('sennitRestApi', ['$http', 'ngAuthSettings', function ($http, ngAuthSettings) {
-
-     function getQueryStringParameter(paramToRetrieve) {
-         var params =
-             document.URL.split("?")[1].split("&");
-         for (var i = 0; i < params.length; i = i + 1) {
-             var singleParam = params[i].split("=");
-             if (singleParam[0] == paramToRetrieve)
-                 return singleParam[1];
-         }
-     }
-
-     var serviceBase =
-                 decodeURIComponent(
-                     getQueryStringParameter("SPAppWebUrl")
-             ).split("#")[0];
+﻿angular.module('sennit.sharejs', [])
+  .factory('sennitRestApi', ['$http', function ($http) {
 
 
-    var serviceBase = ngAuthSettings.apiServiceBaseUri;
+      function getQueryStringParameter(paramToRetrieve) {
+          var params =
+              document.URL.split("?")[1].split("&");
+          for (var i = 0; i < params.length; i = i + 1) {
+              var singleParam = params[i].split("=");
+              if (singleParam[0] == paramToRetrieve)
+                  return singleParam[1];
+          }
+      }
 
-    var sennitRestApiFactory = {};
-
-    var _requestDigest;
-    $http({
-        method: 'POST',
-        url: serviceBase + "/_api/contextinfo",
-        headers: { "Accept": "application/json; odata=verbose" }
-    }).success(function (data) {
-        _requestDigest = data.d.GetContextWebInformation.FormDigestValue
-    });
-
-  
-    
-    var _getSharepointGetUser = function (id, query) {
+      var serviceBase =
+                  decodeURIComponent(
+                      getQueryStringParameter("SPAppWebUrl")
+              ).split("#")[0];
 
 
-        return $http({
-            method: 'GET',
-            url: serviceBase + "_api/Web/GetUserById("+ id +")" + query,
-            headers: { "Accept": "application/json; odata=verbose" }
-        });
-    };
-
-    var _getSharepointListById = function (lista, id, query) {
-        
-        return $http({
-            method: 'GET',
-            url: serviceBase + "/_api/web/lists/GetByTitle('" + lista + "')/items("+ id +")/" + query,
-            headers: { "Accept": "application/json; odata=verbose" }
-        });
-    };
-
-    var _getSharepointList = function (lista, query) {
-        
-        return $http({
-            method: 'GET',
-            url: serviceBase + "/_api/web/lists/GetByTitle('" + lista + "')/items" + query,
-            headers: { "Accept": "application/json; odata=verbose" }
-        });
-    };
-   
-
-    var _getMyProperties = function () {
 
 
-        return $http({
-            method: 'GET',
-            url: serviceBase + "/_api/SP.UserProfiles.PeopleManager/GetMyProperties",
-            headers: { "Accept": "application/json; odata=verbose" }
-        });
-    };
+      var sennitRestApiFactory = {};
+
+      var _requestDigest;
+      $http({
+          method: 'POST',
+          url: serviceBase + "/_api/contextinfo",
+          headers: { "Accept": "application/json; odata=verbose" }
+      }).success(function (data) {
+          _requestDigest = data.d.GetContextWebInformation.FormDigestValue
+      });
 
 
-    
- 
-    sennitRestApiFactory.getMyProperties = _getMyProperties;
-    sennitRestApiFactory.getSharepointGetUser = _getSharepointGetUser;
-    sennitRestApiFactory.getSharepointList = _getSharepointList;
-    sennitRestApiFactory.getSharepointListById = _getSharepointListById;
-    sennitRestApiFactory.requestDigest = _requestDigest;
-    
-    return sennitRestApiFactory;
 
-}]);
+      var _getSharepointGetUser = function (id, query) {
+
+
+          return $http({
+              method: 'GET',
+              url: serviceBase + "/_api/Web/GetUserById(" + id + ")" + query,
+              headers: { "Accept": "application/json; odata=verbose" }
+          });
+      };
+
+      var _getSharepointListById = function (lista, id, query) {
+
+          return $http({
+              method: 'GET',
+              url: serviceBase + "/_api/web/lists/GetByTitle('" + lista + "')/items(" + id + ")/" + query,
+              headers: { "Accept": "application/json; odata=verbose" }
+          });
+      };
+
+      var _getSharepointList = function (lista, query) {
+
+          return $http({
+              method: 'GET',
+              url: serviceBase + "/_api/web/lists/GetByTitle('" + lista + "')/items" + query,
+              headers: { "Accept": "application/json; odata=verbose" }
+          });
+      };
+
+
+
+      var _getMyProperties = function () {
+
+
+          return $http({
+              method: 'GET',
+              url: serviceBase + "/_api/SP.UserProfiles.PeopleManager/GetMyProperties",
+              headers: { "Accept": "application/json; odata=verbose" }
+          });
+      };
+
+
+
+
+
+      sennitRestApiFactory.getMyProperties = _getMyProperties;
+      sennitRestApiFactory.getSharepointGetUser = _getSharepointGetUser;
+      sennitRestApiFactory.getSharepointList = _getSharepointList;
+      sennitRestApiFactory.getSharepointListById = _getSharepointListById;
+      sennitRestApiFactory.requestDigest = _requestDigest;
+
+      return sennitRestApiFactory;
+  }]);
